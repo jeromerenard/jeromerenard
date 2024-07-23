b1a2ddd25a51c24da14af202813eba765c5f4e64
jeromerenard <143679994+jeromerenard@users.noreply.github.com>
2024-07-23 08:31:22 -0700

    v.0.4.2
    update program page - changed price to form

--- src/pages/program/price.tsx
+++ src/pages/program/price.tsx
@@ -44,21 +44,17 @@ export default function Price() {
           <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
-                <p className="text-base font-semibold text-gray-600">Pay once, easy process</p>
-                <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">$1.500</span>
-                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
-                </p>
+                <p className="text-base font-semibold text-gray-600">Limited spots.</p>
+      
                 <a
-                  href="https://buy.stripe.com/5kA6r37FOd2C6gUeV0" target='_blank'
-                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  href="https://forms.gle/pEq6mc5u3dM4bzY37" target='_blank'
+                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-4 text-center text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Register
+                  Apply here
                 </a>
-                <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Invoices and receipts available. <br />
-                  Monthly payment plan available, DM for link.
+                <p className="mt-10 text-xs leading-5 text-gray-600">
+                  Discount code available for the first 10<br />
+                  to apply.
                 </p>
               </div>
               
@@ -69,3 +65,16 @@ export default function Price() {
     </div>
   )
 }
+
+/*
+          <p className="mt-6 flex items-baseline justify-center gap-x-2">
+                  
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">$1.500</span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+                </p>
+
+
+                  Invoices and receipts available. <br />
+                  Monthly payment plan available, DM for link.
+
+                */
